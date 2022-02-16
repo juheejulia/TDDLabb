613310a7f515630da0df8f92d75decd2d442fcff
Juhee Kang Johansson <asmilejuliaz@gmail.com>
2022-02-16 09:24:28 +0100

    Red

--- src/test/java/EmployeeSystemTest.java
+++ src/test/java/EmployeeSystemTest.java
@@ -1,11 +1,14 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class EmployeeSystemTest {
 
-    //findEmployee(by Id)
-    //addEmployee(all obligatorisk info)
-    //removeEmployee
-    //changeAge
-    //changeFirstName
-    //changeLastName
-    //increaseSalary
+    EmployeeSystem employeeSystem;
+
+    @BeforeEach
+    public void beforeEach(){
+        employeeSystem = new EmployeeSystem();
+    }
 
 }
